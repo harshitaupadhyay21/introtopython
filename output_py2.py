78ef03644f73259233aab99be19d80523d471468
harshitaupadhyay21 <96920141+harshitaupadhyay21@users.noreply.github.com>
2022-01-23 22:28:47 +0530

    Add files via upload
    output file

---
+++ output_py2.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:/Users/harsh/Desktop/assgn_py2.py =================
+35
+the reversed string isegaugnal evitisnes esac a si nohtyP
+ a case sensitive
+Python is object oriented language
+10
+Python is a case sensitive language
+
+Hey,Harshita Upadhyay here!
+My SID is 21104047
+I am from Electrical department and my CGPA is 9.9
+
+8
+58
+50
+40
+0
+
+Enter the 1st number:45
+Enter the 2nd number:23
+Enter the 3rd number:67
+the greatest number is: 67
+
+Enter something my name harshita!
+yes
+
+Enter 1st side:12
+Enter 2nd side:13
+Enter 3rd side:5
+Yes, the given sides can form a triangle
+

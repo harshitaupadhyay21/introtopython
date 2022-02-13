63f625800755ce294b3141c9031403a46ce89201
harshitaupadhyay21 <96920141+harshitaupadhyay21@users.noreply.github.com>
2022-02-14 00:19:50 +0530

    Add files via upload
    assignment-3

---
+++ output file-assignment3.py
@@ -0,0 +1,107 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: /Users/harshita/Documents/source code-assignment3.py =====================
+question 1
+Enter a stringfunny
+Enter a character:n
+The total number of times n has occured= 2
+
+
+****************************************************************************************************************************************************************
+question 2
+Enter some year from 1800 to 2025:2024
+Enter a month[1-12]:6
+input a day[1-31]:22
+the next date is [YYYY/MM/DD]2024-6-23.
+
+
+****************************************************************************************************************************************************************
+question 4
+[(2, 4), (5, 25), (4, 16), (8, 64)]
+
+
+****************************************************************************************************************************************************************
+question-4
+Input your grade:9
+Your grade is9
+ and excellentPerformance
+
+****************************************************************************************************************************************************************
+Question-5
+ABCDEFGHIJK
+ABCDEFGHI
+ABCDEFG
+ABCDE
+ABC
+A
+
+
+****************************************************************************************************************************************************************
+Question-6
+Enter your SID:
+21104047
+Enter your name:harshita
+do you want to enter another student entry(y or n):y
+Enter sid:21104046
+Enter name:asmi
+do you want to enter another student entry(y or n):y
+Enter sid:21104045
+Enter name:rahul
+do you want to enter another student entry(y or n):n
+<a>
+ {21104047: 'harshita', 21104046: 'asmi', 21104045: 'rahul'}
+<b>
+ {21104046: 'asmi', 21104047: 'harshita', 21104045: 'rahul'}
+<c
+> {21104045: 'rahul', 21104046: 'asmi', 21104047: 'harshita'}
+Search name with SID for:
+21104047
+<d>
+ harshita
+
+
+****************************************************************************************************************************************************************
+question-7
+Enter a positive integer for the number of terms of fibonacci series:5
+Fibonacci sequence:
+0
+1
+1
+2
+3
+Average of resultant fibonacci series is: 1.4
+
+
+****************************************************************************************************************************************************************
+question-8
+part-a
+<a>
+ {1, 3, 5, 6, 8}
+part-b
+<b>
+ {3, 6, 8, 9, 13, 17}
+<c>
+ {1, 2, 4, 5}
+<d>
+ set()
+<d>
+ set()
+<d>
+ set()
+<d>
+ set()
+<d>
+ set()
+<d>
+ {6}
+<d>
+ {6, 7}
+<d>
+ {8, 6, 7}
+<d>
+ {8, 9, 6, 7}
+<d>
+ {6, 7, 8, 9, 10}
+<e>
+ {10}

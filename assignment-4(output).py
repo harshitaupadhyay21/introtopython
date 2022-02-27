eea1338ce7b4c24982236c822d404ba2b826688c
harshitaupadhyay21 <96920141+harshitaupadhyay21@users.noreply.github.com>
2022-02-27 21:17:18 +0530

    Add files via upload
    assignment-4(output)

---
+++ assignment-4(output).py
@@ -0,0 +1,85 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: /Users/harshita/Desktop/assignment-4(source code).py ========
+******QUESTION-1:******
+Move disk 1 from rod A to rod B
+Move disk 2 from rod A to rod C
+Move disk 1 from rod B to rod C
+Move disk 3 from rod A to rod B
+Move disk 1 from rod C to rod A
+Move disk 2 from rod C to rod B
+Move disk 1 from rod A to rod B
+/n
+*******QUESTION-2*******
+Enter number of rows:5
+            1 
+/n
+          1   1 
+/n
+        1   2   1 
+/n
+      1   3   3   1 
+/n
+    1   4   6   4   1 
+/n
+*******QUESTION-3:*******
+Enter first integer:20
+Enter second integer:6
+
+(Quotient, Remainder) is (3, 2)
+
+PART A Function is callable or not:  False
+
+PART B
+Since, multiplication of elements of a list is non-zero, None of the elements is zero.
+
+PART C
+List after adding new values is [3, 2, 4, 5, 6]
+List after filtering out values greater than 4 is [3, 2, 4]
+
+PART D: convert to set
+{2, 3, 4}
+
+PART E: make set immutable
+frozenset({2, 3, 4})
+
+PART D: find max value and find its hash
+The maximum value from the set is  4
+Hash value of the max value is  4
+-------------
+********QUESTION -4:*******
+Making an object...
+Student Class created.
+The name of the student is Sample_Name and Roll Number is 123
+Function end...
+Program ended.
+******QUESTION-5:******
+Details of Employees:
+1 Mehak 40000
+2 Ashok 50000
+3 Viren 60000
+------
+
+PART A
+1 Mehak 70000
+
+PART B - refer to code as no output had to be printed for this part.
+******QUESTION-6:******
+Enter the word from friend 1:hurray
+Enter the word from friend 2:good
+ :( 
+
+ :( 
+
+ :( 
+
+ :( 
+
+ :( 
+
+
+Press N if any of the above result was :( , otherwise press Y  ---- N
+
+
+Fake friendship! Don't worry, this is for fun!
